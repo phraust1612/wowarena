80af5f3b8b061c362061e9be1226d6e6e0134309
Min ByeongUk <phraust@kaist.ac.kr>
2017-03-22 20:01:10 +0900

    update

--- wowarena.py
+++ wowarena.py
@@ -7,8 +7,9 @@ import requests as rq
 class wowLadder(QWidget):
     def __init__(self):
         super().__init__()
+        self.mode = 0
+        self.totalNo = 0
         self.initUI()
-        self.mode = 1
         self.apikey = "ks3xtugk5fdjdvp45h9yyq7756jszzg2"
         self.Reload()
     def initUI(self):
@@ -23,21 +24,38 @@ class wowLadder(QWidget):
         self.choice.addItem("2v2")
         self.choice.addItem("3v3")
         self.choice.addItem("평전")
-        self.choice.addItem("연투")
         self.choice.move(50,50)
         self.choice.activated[str].connect(self.Choose)
 
         self.fr = []
         self.tx = []
+        self.pg = []
         for i in range(0,10):
             self.fr.append(QFrame(self))
             self.tx.append(QLabel(self))
+            self.pg.append(QPushButton(self))
             self.fr[i].resize(500,48)
             self.fr[i].move(200,20 + 50*i)
-            self.tx[i].move(220,28 + 50*i)
+            self.tx[i].move(220,30 + 50*i)
+            self.pg[i].resize(20,20)
+            self.pg[i].move(215 + 50*i, 520)
+            self.pg[i].setText(str(i+1))
+            self.pg[i].setStyleSheet("border: 0px")
             self.fr[i].setStyleSheet("QFrame { background-color: white; border: 1px solid black }")
-        self.ShowPage(0)
+        self.pg[0].clicked.connect(lambda: self.PageChoose(0))
+        self.pg[1].clicked.connect(lambda: self.PageChoose(1))
+        self.pg[2].clicked.connect(lambda: self.PageChoose(2))
+        self.pg[3].clicked.connect(lambda: self.PageChoose(3))
+        self.pg[4].clicked.connect(lambda: self.PageChoose(4))
+        self.pg[5].clicked.connect(lambda: self.PageChoose(5))
+        self.pg[6].clicked.connect(lambda: self.PageChoose(6))
+        self.pg[7].clicked.connect(lambda: self.PageChoose(7))
+        self.pg[8].clicked.connect(lambda: self.PageChoose(8))
+        self.pg[9].clicked.connect(lambda: self.PageChoose(9))
+        self.pg[0].setStyleSheet("color: red; border: 0px")
+        self.currentPageNo = 0
         self.show()
+        self.mode = 1
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
@@ -46,15 +64,20 @@ class wowLadder(QWidget):
     def Choose(self,text):
         if text == "2v2":
             self.mode = 1
-            self.ShowPage(0)
         elif text == "3v3":
             self.mode = 2
-            self.ShowPage(0)
         elif text == "평전":
             self.mode = 3
-            self.ShowPage(0)
         else:
             self.mode = 0
+            return
+        self.Reload()
+    def PageChoose(self,no):
+        self.pg[no].setStyleSheet("color: red; border: 0px")
+        self.pg[self.currentPageNo].setStyleSheet("color: black; border: 0px")
+        self.currentPageNo = no
+        i = int(self.pg[no].text())
+        self.ShowPage(i)
     def Reload(self):
         url = ""
         if self.mode == 1:
@@ -69,22 +92,35 @@ class wowLadder(QWidget):
         res = rq.get(url)
         if res.status_code >= 200 and res.status_code<300:
             self.ladder = res.json()
+            self.totalNo = len(self.ladder['rows'])
+            self.maxPageNo = int((self.totalNo-1)/10)+1
+            self.ShowPage(1)
             return 0
         else:
             return -2
     def ShowPage(self,no):
-        for i in range(0,10):
+        try:
+            self.ladder
+        except AttributeError:
+            return
+        i = no - 1
+        i *= 10
+        count = 0
+        while count<10 and i<self.totalNo:
             tmp = "랭킹 "
             tmp += str(self.ladder['rows'][i]['ranking'])
             tmp += " 위      레이팅 : "
             tmp += str(self.ladder['rows'][i]['rating'])
             tmp += "\n서버 : "
             tmp += self.ladder['rows'][i]['realmName']
-            tmp += "    "
+            tmp += "          "
             tmp += self.ClassName(self.ladder['rows'][i]['classId'])
             tmp += "    "
             tmp += self.ladder['rows'][i]['name']
-            self.tx[i].setText(tmp)
+            self.tx[count].setText(tmp)
+            self.tx[count].adjustSize()
+            count += 1
+            i += 1
     def ClassName(self, no):
         if no == 1:
             return "전사"
